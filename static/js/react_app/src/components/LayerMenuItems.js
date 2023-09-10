e5bcd260efe2a54390302416e4686077073272ad
Bwalker <brettwalker1337@gmail.com>
2023-09-10 10:39:26 -0500

    Changes to be committed: 	new file:   static/js/react_app/src/components/LayerMenuItems.js

---
+++ static/js/react_app/src/components/LayerMenuItems.js
@@ -0,0 +1,38 @@
+export const LayerMenuItems = [
+    {
+        title: 'Dense',
+    }
+
+    {
+        title: 'Special',
+    }
+
+    {
+        title: 'Convolutional',
+    }
+
+    {
+        title: 'Pooling',
+    }
+
+    {
+        title: 'Recurrent',
+    }
+
+    {
+        title: 'Preprocessing',
+    }
+
+    {
+        title: 'Normalization',
+    }
+
+    {
+        title: 'Regularization',
+    }
+
+    {
+        title: 'Other',
+    }
+
+];
